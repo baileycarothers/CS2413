f8b4a0fc5172cc7a075e17a8786b223c353e0b2c
baileycarothers <44585056+baileycarothers@users.noreply.github.com>
2019-02-04 10:48:06 -0600

    Lab updates

--- Lab 01/Adder.cpp
+++ Lab 01/Adder.cpp
@@ -1,17 +1,73 @@
 #include "Adder.h"
-/*
-void Adder::add()
-{
 
+//Standard initialization to 2 and 2
+Adder::Adder()
+{
+	first=2;
+	second=2;
 }
-
-int Adder::get(int a)
+//Allows class initialization to 2 numbers of choice
+Adder::Adder(int x, int y)
 {
-	if(a == 1)
+	first = x;
+	second = y;
+}
+//Prints the two numbers within the class to the user
+void Adder::printNums()
+{
+	cout << "The two numbers are " << first << " and " << second << endl;
+}
+//Unused, allows the user to set both numbers manually
+void Adder::newNums()
+{
+	cout << "Please enter the first number: ";
+	cin >> first;
+	cout << "Please enter the second number: ";
+	cin >> second;
+}
+//Adds the two numbers within the class
+int Adder::add()
+{
+	cout << first << "+" << second << "=" << first + second << endl;
+	return first + second;
+}
+//Subtracts the two numbers within the class
+int Adder::subtract()
+{
+	cout << first << "-" << second << "=" << first - second << endl;
+	return first - second;	
+}
+//Multiplies the two numbers within the class
+int Adder::multiply()
+{
+	cout << first << "*" << second << "=" << first * second << endl;
+	return first * second;	
+}
+//Divides the two numbers within the class
+int Adder::divide()
+{
+	cout << first << "/" << second << "=" << first / second << endl;
+	return first / second;	
+}
+//Gets the number selected by the function call
+int Adder::get(int selection)
+{
+	if(selection == 1)
+	{
+		cout << first << endl;
 		return first;
-	else if(a == 2)
+	}
+	else if(selection == 2)
+	{
+		cout << second << endl;
 		return second;
-	else
-		cout << "Invalid selection" << endl;
+	}
 }
-*/
+//Changes the number selected by the function call to the second number in the function call
+void Adder::set(int selection, int newNum)
+{
+	if(selection == 1)
+		first = newNum;
+	else if(selection == 2)
+		second = newNum;
+}
--- Lab 01/main.cpp
+++ Lab 01/main.cpp
@@ -4,10 +4,21 @@
 int main()
 
 {
-	Adder obj1; //Declaring obj1 of class Adder
+	//For our first Adder class, we initiate it with no variables
+	//Which defaults it to 2 and 2
+	Adder adder1; //Declaring obj1 of class Adder
+	adder1.printNums(); //Print the numbers
+	//Time to perform all operations to the first adder
+	adder1.add();
+	adder1.subtract();
+	adder1.multiply();
+	adder1.divide();
+	adder1.get(1); //Return the number in the first slot
+	adder1.set(1,9); //Set the number in the first slot to 9
+	adder1.printNums(); //Print out our numbers again to verify the change
 
-	obj1.printNums();
-	obj1.newNums();
-	obj1.printNums();
+	//Declare a second adder initialized to 6 and 9
+	Adder adder2(6,9);
+	adder2.printNums(); //Print nums back out to check for correct initialization
 	return 0;
 }
